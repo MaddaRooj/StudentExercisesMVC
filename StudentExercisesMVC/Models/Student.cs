83cfec6217e7ebee7aa22913398eaf423d060dfc
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-08-13 10:30:27 -0500

    first commit

---
+++ StudentExercisesMVC/Models/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesMVC.Models
+{
+    public class Student
+    {
+        [Required]
+        [Display(Name = "StudentId")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "FirstName")]
+
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "LastName")]
+
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "SlackHandle")]
+
+        public string SlackHandle { get; set; }
+
+        [Required]
+        [Display(Name = "CohortId")]
+
+        public int CohortId { get; set; }
+
+        [Required]
+        [Display(Name = "FullName")]
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
